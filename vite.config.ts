441a4394cca9bc1d1662a7e6b57e6dff5df55d5f
Vincenzo Marrazzo <72307974+xzan8189@users.noreply.github.com>
2025-08-24 14:55:25 +0200

    fix: nome del repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  // 👇 Sostituisci "NOME-DEL-REPO" con il tuo nome repo preciso
+  base: "/gpu-forum-web/",
+})
